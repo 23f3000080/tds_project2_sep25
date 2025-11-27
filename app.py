7fb0659e5b551952fd6fd17dbcb23066bda98c2b
23f3000080 <23f3000080@ds.study.iitm.ac.in>
2025-11-27 07:48:18 +0530

    gunicorn added

--- app.py
+++ app.py
@@ -791,4 +791,5 @@ def test():
     return jsonify({"status": "Server is running", "email": YOUR_EMAIL}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
